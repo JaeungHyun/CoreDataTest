10f177ce29f5dc30dae5dba28d1a09d7be8fd38a
JaeUng Hyun <jaeung@me.com>
2019-05-10 13:29:23 +0900

    Add logViewController to show log

--- testcoredata/ListVC.swift
+++ testcoredata/ListVC.swift
@@ -200,5 +200,13 @@ class ListVC: UITableViewController {
         }) // end of alertAddAction
         self.present(alert, animated: false)
     }
+    
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        let object = self.list[indexPath.row]
+        let uvc = self.storyboard?.instantiateViewController(withIdentifier: "LogVC") as! LogVC
+        uvc.board = object as! BoardMO
+        
+        self.show(uvc, sender: self)
+    }
 }
 
--- testcoredata/LogVC.swift
+++ testcoredata/LogVC.swift
@@ -10,7 +10,44 @@ import UIKit
 import CoreData
 
 class LogVC: UITableViewController {
+    var board: BoardMO! // 게시글 정보를 전달받을 변수
     
+    lazy var list: [LogMO]! = {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest: NSFetchRequest<LogMO> = LogMO.fetchRequest()
+        let predict = NSPredicate(format: "board == %@", self.board)
+        fetchRequest.predicate = predict
+        
+        let sort = NSSortDescriptor(key: "regDate", ascending: true)
+        fetchRequest.sortDescriptors = [sort]
+        
+        do {
+            return try context.fetch(fetchRequest)
+        } catch let error as NSError {
+            NSLog("An error has occurred while list : %@, %@", error, error.userInfo)
+            return []
+        }
+    }()
+    
+    override func viewDidLoad() {
+        self.navigationItem.title = self.board?.title
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = self.list[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "logcell")!
+        cell.textLabel?.text = "\(row.regDate!)에 \(row.type.toLogType())되었습니다"
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 12)
+        
+        return cell
+    }
 }
 
 public enum LogType: Int16 {
